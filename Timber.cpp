8d099c2b8ff2bb6c28d3f6da9bb63735548be440
Aditi7978 <146857150+Aditi7978@users.noreply.github.com>
2025-03-27 10:41:05 +0530

    Add files via upload

---
+++ Timber.cpp
@@ -0,0 +1,410 @@
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <sstream>
+// Make code easier to type with "using namespace"
+using namespace sf;
+//Function declaration
+void updateBranches(int seed);
+
+const int NUM_BRANCHES=6;
+Sprite branches[NUM_BRANCHES];
+
+//position of branches,left or right
+enum class side{LEFT,RIGHT,NONE};
+side branchPosition[NUM_BRANCHES];
+int main()
+{
+	// Create a video mode object
+	// VideoMode vm(1920, 1080);
+
+	// Low res code
+	VideoMode vm(960, 540);
+
+	// Create and open a window for the game
+	// RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	// Low res code
+	RenderWindow window(vm, "Timber!!!");
+	View view(sf::FloatRect(0, 0, 1920, 1080));
+	window.setView(view);
+	//End of low res code!! Everything else is the same!!
+
+	// Create a texture to hold a graphic on the GPU
+	Texture textureBackground;
+
+	// Load a graphic into the texture
+	textureBackground.loadFromFile("/home/student/Desktop/gpwc_2241004021/Project1 (1)/graphics/background.png");
+
+	// Create a sprite
+	Sprite spriteBackground;
+
+	// Attach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+
+	// Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
+
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("/home/student/Desktop/gpwc_2241004021/Project1 (1)/graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
+	// Prepare the bee
+	Texture textureBee;
+	textureBee.loadFromFile("/home/student/Desktop/gpwc_2241004021/Project1 (1)/graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+
+	// Is the bee currently moving?
+	bool beeActive = false;
+
+	// How fast can the bee fly
+	float beeSpeed = 0.0f;
+	
+	// make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+
+	// Load 1 new texture
+	textureCloud.loadFromFile("/home/student/Desktop/gpwc_2241004021/Project1 (1)/graphics/cloud.png");
+
+	// 3 New sprites withe the same texture
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	// Position the clouds off screen
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 150);
+	spriteCloud3.setPosition(0, 300);
+
+	// Are the clouds currently on screen?
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+
+	// How fast is each cloud?
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	// Variables to control time itself
+	Clock clock;
+
+	// Time Bar
+    RectangleShape timeBar;
+    float timeBarStartWidth = 400;
+    float timeBarHeight = 40;
+    timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+    timeBar.setFillColor(Color::Red);
+    timeBar.setPosition((1250 / 2) - timeBarStartWidth / 2, 600);
+
+    float timeRemaining = 8.0f;
+    float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+    bool paused = true;
+
+	int score=0;
+	sf::Text scoreText;
+	sf::Text messageText;
+	sf::Font font;
+	font.loadFromFile("/home/student/Desktop/gpwc_2241004021/Project1 (1)/fonts/KOMIKAP_.ttf");
+	
+	//set the font to the msg
+	scoreText.setFont(font);
+	messageText.setFont(font);
+
+	//set the specific msg
+	scoreText.setString("score=0");
+	messageText.setString("Press Enter to Start!!!");
+
+	//set size
+	scoreText.setCharacterSize(100);
+	messageText.setCharacterSize(200);
+
+	//set color
+	scoreText.setFillColor(Color::White);
+	messageText.setFillColor(Color::White);
+
+	//set position
+	scoreText.setPosition(20,20);
+	FloatRect textRect=messageText.getLocalBounds();
+	messageText.setOrigin(textRect.left+ textRect.width/2.0f,textRect.top+ textRect.height/2.0f);
+	messageText.setPosition(1920/2.0f,1080/2.0f);
+
+	// create 5 branches
+	Texture textureBranch;
+
+	textureBranch.loadFromFile("/home/student/Desktop/gpwc_2241004021/Project1 (1)/graphics/branch.png");
+	
+	//set the texture for each branch sprite
+	for(int i=0;i<NUM_BRANCHES;i++){
+		branches[i].setTexture(textureBranch);
+		branches[i].setPosition(-2000,-2000);
+
+		//set the sprite origin to centre
+		branches[i].setOrigin(220,20);
+	}
+	updateBranches(1);
+	updateBranches(2);
+	updateBranches(3);
+	updateBranches(4);
+	updateBranches(5);
+	
+	//main loop
+	while (window.isOpen())
+	{		
+		/*
+		****************************************
+		Handle the players input
+		****************************************
+		*/
+
+		if (Keyboard::isKeyPressed(Keyboard::Return)) {
+            paused = false;
+			//reset the time and score
+			score=0;
+			timeRemaining=6;
+			//messageText.setString("");
+        }
+		if (!paused) {
+            Time dt = clock.restart();
+            timeRemaining -= dt.asSeconds();
+            timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBarHeight));
+
+            if (timeRemaining < 0.0f) {
+				paused = true;
+				//change the messgae shown to player
+				messageText.setString("Out of Time!!");
+
+				FloatRect textRect=messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left+ textRect.width/2.0f,
+				textRect.top+ textRect.height/2.0f);
+				messageText.setPosition(1920/2.0f,1080/2.0f);
+                
+            }
+		
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		/*/home/student/Desktop/gpwc_2241004021/Project1 (1)/graphics/branch.png
+		*/
+		// Measure time
+		//Time dt = clock.restart();
+
+		// Setup the bee
+		if (!beeActive)
+		{
+
+			// How fast is the bee
+			srand((int)time(0) * 10);
+			beeSpeed = (rand() % 200) + 200;
+			
+			// How high is the bee
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+			beeActive = true;
+
+		}
+		else
+			// Move the bee
+		{
+
+			spriteBee.setPosition(
+				spriteBee.getPosition().x -
+				(beeSpeed * dt.asSeconds()),
+				spriteBee.getPosition().y);
+
+			// Has the bee reached the right hand edge of the screen?
+			if (spriteBee.getPosition().x < -100)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				beeActive = false;
+			}
+		}
+
+		// Manage the clouds
+		// Cloud 1
+		if (!cloud1Active)
+		{
+
+			// How fast is the cloud
+			srand((int)time(0) * 10);
+			cloud1Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 10);
+			float height = (rand() % 150);
+			spriteCloud1.setPosition(-200, height);
+			cloud1Active = true;
+
+
+		}
+		else
+		{
+
+			spriteCloud1.setPosition(
+				spriteCloud1.getPosition().x +
+				(cloud1Speed * dt.asSeconds()),
+				spriteCloud1.getPosition().y);
+
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud1.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud1Active = false;
+			}
+		}
+		// Cloud 2
+		if (!cloud2Active)
+		{
+
+			// How fast is the cloud
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(-200, height);
+			cloud2Active = true;
+
+
+		}
+		else
+		{
+
+			spriteCloud2.setPosition(
+				spriteCloud2.getPosition().x +
+				(cloud2Speed * dt.asSeconds()),
+				spriteCloud2.getPosition().y);
+
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud2Active = false;
+			}
+		}
+
+		if (!cloud3Active)
+		{
+
+			// How fast is the cloud
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(-200, height);
+			cloud3Active = true;
+
+
+		}
+		else
+		{
+
+			spriteCloud3.setPosition(
+				spriteCloud3.getPosition().x +
+				(cloud3Speed * dt.asSeconds()),
+				spriteCloud3.getPosition().y);
+
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud3Active = false;
+			}
+		}
+
+		std::stringstream ss;
+		ss<<"score="<<score;
+		scoreText.setString(ss.str());
+
+		//update the branch sprite
+		for(int i=0;i<NUM_BRANCHES;i++){
+			float height=i*150;
+			if(branchPosition[i]==side::LEFT)
+			{	//move the sprite to the left
+				branches[i].setPosition(600,height);
+				branches[i].setRotation(180);
+			}
+			else if(branchPosition[i]==side::RIGHT)
+			{
+				branches[i].setPosition(1330,height);
+				branches[i].setRotation(0);	
+			}
+			else
+			{	//hide the branch
+				branches[i].setPosition(3000,height);
+			}
+		}
+
+	}
+		/*
+		****************************************
+		Draw the scene
+		****************************************
+		*/
+
+		// Clear everything from the last frame
+		window.clear();
+
+		// Draw our game scene here
+		window.draw(spriteBackground);
+
+		// Draw the clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+		for(int i=0;i<NUM_BRANCHES;i++){
+			window.draw(branches[i]);
+		}
+		window.draw(spriteTree);
+		window.draw(spriteBee);
+		window.draw(scoreText);
+		window.draw(timeBar);
+		if(paused){
+			window.draw(messageText);
+		}
+		
+		window.display();
+
+
+		
+}
+		return 0;
+}
+//function definition
+void updateBranches(int seed){
+	//move the branches down one place
+	for(int j=NUM_BRANCHES-1;j>0;j--){
+		branchPosition[j]=branchPosition[j-1];
+	}
+		//position LEFT,RIGHT or NONE
+		srand((int)time(0)+seed);
+		int r=(rand()%5);
+		switch(r){
+			case 0:
+				branchPosition[0]=side::LEFT;
+				break;
+			case 1:
+				branchPosition[0]=side::RIGHT;
+				break;
+			default:
+				branchPosition[0]=side::NONE;
+				break;
+		}
+	
+}
+
+
